d37bf20dbaa8c75d5a4ae1d199d324b5aaff94e3
Melissa Augier <augiermelissa@gmail.com>
2020-09-29 11:12:17 +0200

    categories index begin

--- app/javascript/stylesheets/tailwind.js
+++ app/javascript/stylesheets/tailwind.js
@@ -244,22 +244,17 @@ module.exports = {
     },
     fontFamily: {
       sans: [
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
+        '"Assistant"',
+        '"Cabin"',
         'sans-serif',
         '"Apple Color Emoji"',
         '"Segoe UI Emoji"',
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
-      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+      cursive: [
+        'Cabin Sketch',
+      ],
     },
     fontSize: {
       xs: '0.75rem',
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   belongs_to :category
   has_many :document_tags, dependent: :destroy
+  has_many :documents, through: :document_tags
 end
