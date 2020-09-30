941b998eb6cfcec9d0dbefcf5d34d5a4daaeae98
Marceau Tassin <marceau.tassin@gmail.com>
2020-09-30 03:14:43 +0200

    tiny change on DocumentsController test

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -3,28 +3,28 @@ require 'pry-byebug'
 
 RSpec.describe DocumentsController, type: :controller do
 
-  context "with user logged in" do
-    describe "GET #index" do
-      login_user
-      let(:john) {create(:user)}
+  describe "GET #index" do
+    let(:laetitia) {create(:user)}
+    let(:john) {create(:user)}
 
-      let(:valid_attributes) {{name: "test", user: @user, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
-      let(:valid_attributes2) {{name: "test", user: @user, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
-      let(:valid_attributes3) {{name: "test", user: john, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
-      let(:valid_session) {{}}
-      it 'returns a success response' do
-        Document.create! valid_attributes
-        get :index, params: {}, session: valid_session
-        expect(response).to be_successful
-      end
-      it 'returns all the document linked to the user' do
-        Document.create! valid_attributes
-        Document.create! valid_attributes2
-        Document.create! valid_attributes3
-        expect(Document.all.length).to eq(3)
-        expect(@user.documents.all.length).to eq(2)
-        expect(john.documents.all.length).to eq(1)
-      end
+    let(:valid_attributes) {{name: "test", user: laetitia, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
+    let(:valid_attributes2) {{name: "test", user: laetitia, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
+    let(:valid_attributes3) {{name: "test", user: john, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
+    let(:valid_session) {{}}
+    it 'it works' do
+      Document.create! valid_attributes
+      get :index, params: {}, session: valid_session
+      expect(response).to be_successful
+      expect(response).to render_template("index")
+    end
+
+    it 'returns only documents linked to the user' do
+      Document.create! valid_attributes
+      Document.create! valid_attributes2
+      Document.create! valid_attributes3
+      expect(Document.all.length).to eq(3)
+      expect(laetitia.documents.all.length).to eq(2)
+      expect(john.documents.all.length).to eq(1)
     end
   end
 end
