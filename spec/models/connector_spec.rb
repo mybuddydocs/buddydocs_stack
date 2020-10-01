e69e9aaac041614385ce765864f731d0fa7a3241
Marceau Tassin <marceau.tassin@gmail.com>
2020-10-01 12:48:02 +0200

    Force Travis

--- spec/models/connector_spec.rb
+++ spec/models/connector_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Connector, type: :model do
-  context 'General settings' do
     it "is not valid without a name" do
       connector = Connector.new( name: nil)
       expect(connector).to_not be_valid
@@ -11,5 +10,4 @@ RSpec.describe Connector, type: :model do
       expect(connector).to be_valid
     end
     it 'has many credentials'
-  end
 end
