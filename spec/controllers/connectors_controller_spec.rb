41b8bfe3ddf08a4158a9dd583759a81272c8f46a
Marceau Tassin <marceau.tassin@gmail.com>
2020-10-02 17:23:46 +0200

    test on controllers connector and documents

--- spec/controllers/connectors_controller_spec.rb
+++ spec/controllers/connectors_controller_spec.rb
@@ -2,26 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ConnectorsController, type: :controller do
 
-  describe "When user is logged in" do
-    let(:valid_attributes1) {{name: "test1", url: "www.test1.fr"}}
-    let(:valid_attributes2) {{name: "test2", url: "www.test2.fr"}}
-    let(:valid_attributes3) {{name: "test3", url: "www.test2.fr", available: false}}
-    let(:valid_session) {{}}
-    context "GET #index" do
-      before do
-        Connector.create! valid_attributes1
-        Connector.create! valid_attributes2
-        Connector.create! valid_attributes3
-      end
-    it 'it works' do
-      Connector.create! valid_attributes1
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-      expect(response).to render_template("index")
-    end
-      it 'display all connectors available' do
-        expect(Connector.all.length).to eq(3)
-        expect(Connector.where(available: true).length).to eq(2)
+  let(:connectors) {create_list(:connector, 10)}
+  let(:laetitia) {create(:user)}
+  describe "GET #index" do
+  it 'it works' do
+    get :index
+    expect(response).to be_successful
+    expect(response).to render_template("index")
+  end
+    it 'should list all connectors' do
+      sign_in laetitia
+      redirect_to connectors_path
+
+      connectors.each do |connector|
+        expect(connector.name).to be_truthy
       end
     end
   end
--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe DocumentsController, type: :controller do
   describe "GET #index" do
     let(:laetitia) {create(:user)}
     let(:documents) {create_list(:document, 5, user: laetitia)}
+    it 'should get index' do
+      sign_in laetitia
+      get :index
+
+      expect(response).to have_http_status(200)
+      expect(response).to render_template("index")
+    end
     it 'should list all documents' do
       sign_in laetitia
       redirect_to documents_path
