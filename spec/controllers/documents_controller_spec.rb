c86b54c8ebe9209d7d3753e673b3264fea0376c8
Marceau Tassin <marceau.tassin@gmail.com>
2020-10-02 17:01:48 +0200

    bad commit on master

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -5,26 +5,14 @@ RSpec.describe DocumentsController, type: :controller do
 
   describe "GET #index" do
     let(:laetitia) {create(:user)}
-    let(:john) {create(:user)}
+    let(:documents) {create_list(:document, 5, user: laetitia)}
+    it 'should list all documents' do
+      sign_in laetitia
+      redirect_to documents_path
 
-    let(:valid_attributes) {{name: "test", user: laetitia, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
-    let(:valid_attributes2) {{name: "test", user: laetitia, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
-    let(:valid_attributes3) {{name: "test", user: john, url: "www.test.fr", date: "Sept. 29, 2020", origin: "test"}}
-    let(:valid_session) {{}}
-    it 'it works' do
-      Document.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-      expect(response).to render_template("index")
-    end
-
-    it 'returns only documents linked to the user' do
-      Document.create! valid_attributes
-      Document.create! valid_attributes2
-      Document.create! valid_attributes3
-      expect(Document.all.length).to eq(3)
-      expect(laetitia.documents.all.length).to eq(2)
-      expect(john.documents.all.length).to eq(1)
+      documents.each do |document|
+        expect(document.name).to be_truthy
+      end
     end
   end
 end
