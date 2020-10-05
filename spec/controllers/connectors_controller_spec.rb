b33e4cc5fabba1ebe6d0bd41ee2ea99c2bd06f86
Marceau Tassin <marceau.tassin@gmail.com>
2020-10-05 13:06:20 +0200

    comment render_views

--- spec/controllers/connectors_controller_spec.rb
+++ spec/controllers/connectors_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'pry-byebug'
 
 RSpec.describe ConnectorsController, type: :controller do
-render_views
+# render_views
   let(:connectors) {create_list(:connector, 3)}
   let(:laetitia) {create(:user)}
   describe "GET #index" do
