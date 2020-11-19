a26e687d1391585317e4bc9972b85a71cf5ae039
Quentin De Malherbe <quentdemalherbe@gmail.com>
2020-11-19 11:36:49 +0100

    je sais plus

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -7,43 +7,31 @@ class DocumentsController < ApplicationController
   end
 
   def new
+    @document= Document.new
+        #  BUG: content et page_number pas pris en compte dans le build (mettre des inputs caché)
+    @document.pages.build(content: 'en cours de traitement', page_number: 1)
+    @document.document_tags.build
   end
 
   def create
     # ici enregistre a la fois page new et aussi document_tag egalement
     # et ocr ...
-    # pb a regler: les messages d'erreur,
-                  # afficher la photo a upload ,
+    # pb a regler:
+                  # message erreur , probleme de render du else
                   # les couleurs... ,
-                  # arriver sur categories index,
-                  # relancer la machine etc...
-                  # flex space around
+                  # arriver sur categories index ( bug mais du a des probleme d'enregistrer des dates )
                   #  refiler caméra
-                  #  date du doc, ocr ou uplaod?
     @document = Document.new(document_params)
+
     @document.user = current_user
     @document.url = "pc"
     @document.origin="pc"
     @document.generated_date = Date.today
-
-    @document_tag = DocumentTag.new(document_params[:document_tags_attributes])
-
-    @page = Page.new(document_params[:pages_attributes])
-    @page.content ='en cours de traitement'
-    @page.page_number = 1
-    @page.document = @document
-    @page.photo.attach(params["document"]["page"]["photo"])
-    @page.save!
-    #  l'ocr est fait par google dans l'after commit de page
-    @document_tag.document = @document
-    @document_tag.tag = Tag.find(params["document"]["document_tag"]["tag"])
-    @document_tag.save!
-    if @document.save!
-      redirect_to categories_path
+    if @document.save
+      render 'categories/index'
     else
-      render 'new'
+      render 'documents/new'
     end
-
   end
 
   def edit
@@ -57,6 +45,7 @@ class DocumentsController < ApplicationController
 
   private
   def document_params
-    params.require(:document).permit(:name, :url, :generated_date, :origin, pages_attributes: [:photo], document_tags_attributes: [:tag] )
+    params.require(:document).permit(:name, pages_attributes: [:id, :photo], document_tags_attributes: [:id, :tag_id])
+
   end
 end
--- app/worker/ocr_worker.rb
+++ app/worker/ocr_worker.rb
@@ -3,6 +3,7 @@ class OcrWorker < ApplicationJob
 
   def perform(page_id)
     page = Page.find(page_id)
+    photoUploaded = Cloudinary::Search.expression("public_id:#{page.photo.key}").execute
     puts 'test sidekiq'
   end
 end
